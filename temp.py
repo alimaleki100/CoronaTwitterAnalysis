a53efc0044896d1130c4ad3e95349fd717aee0be
Yashar Eskandari <yashar.eskandari@gmail.com>
2020-01-28 00:57:12 +0330

    trying to keep the tweets on error
    This way we can get the tweets one by one and store each one as we go. but it is limited and slow.

--- temp.py
+++ temp.py
@@ -1,15 +1,11 @@
-# -- coding: utf-8 --
+# -*- coding: utf-8 -*-
 """
-Created on Sun Jan 26 14:32:39 2020
+Created on Mon Jan 27 23:59:09 2020
 
-@author: alimaleki100
+@author: Yashar
 """
-
-
-import tweepy as tp
 import pandas as pd
-
-
+import tweepy
 
 access_token='46845259-aFZybrNn3gTgBCcvbxo3uVkuopgRK9Lx3zg43xoEd'
 access_token_secret='M4nJxQ6nfB1AXEjsn1zkC62tOH9haQG0rDZAOd0PZaB9a'
@@ -17,32 +13,32 @@ apiKey='34oHUFxZ8WvnzthuJ010BGPgW'
 apiSecretKey='2XUUm5vz54b9UcjXqBYf0oRYwwW5r860NLVCSIKRS85Fcgc7F7'
 
 # Creating the authentication object
-auth = tp.OAuthHandler(apiKey, apiSecretKey)
+auth = tweepy.OAuthHandler(apiKey, apiSecretKey)
 # Setting your access token and secret
 auth.set_access_token(access_token, access_token_secret)
 # Creating the API object while passing in auth information
-api = tp.API(auth,wait_on_rate_limit=True) 
+api = tweepy.API(auth,wait_on_rate_limit=True) 
 
-dfcolumns=['coordinates','source','text','lan','screen_name','userlocation','place','retweetCount','created_at','id','userName',
-           'in_reply_to_status_id','in_reply_to_status_id_str','in_reply_to_user_id','in_reply_to_user_id_str',
-           'in_reply_to_screen_name','user.id_str','user.created_at','user.geo_enabled','favorite_count','retweet_count','entities']
 
-""""
-coordinates.coordinates
-place.bounding_box.coordinates
-place.country
-place.country_code
-place.full_name
-place.id
-place.name
-place.type
-"""
-
-tweets = tp.Cursor(api.search,
-              q='corona',
-                           since = "2020-01-23",
-                           until = "2020-01-24").items()
-
+searched_tweets = []
+max_tweets = 100
+last_id = -1
+c = 0
+while len(searched_tweets) < max_tweets:
+    count = max_tweets - len(searched_tweets)
+    try:
+        new_tweets = api.search(q='corona', count=count, max_id=str(last_id - 1))
+        if not new_tweets:
+            break
+        searched_tweets.extend(new_tweets)
+        c = c + 1
+        print(c)
+        last_id = new_tweets[-1].id
+    except tweepy.TweepError as e:
+        # depending on TweepError.code, one may want to retry or wait
+        # to keep things simple, we will give up on an error
+        print(e)
+        break
 
 tweet_data=[[tweet.coordinates,
              tweet.source,
@@ -65,11 +61,4 @@ tweet_data=[[tweet.coordinates,
              tweet.user.geo_enabled,
              tweet.favorite_count,
              tweet.retweet_count,
-             tweet.entities] for tweet in tweets]
-
-
-tweetdf=pd.DataFrame(data=tweet_data,columns=dfcolumns)
-writer=pd.ExcelWriter("C:/Users/session1/Desktop/CoronaVirus24.xlsx",engine='xlsxwriter')
-tweetdf.to_excel(writer, sheet_name='new')
-writer.close()
-
+             tweet.entities] for tweet in new_tweets]    
